d81af2d34e54a2f576dc4fd72af726c32728362f
andreyliu <yuqiliu@andrew.cmu.edu>
2019-12-07 01:35:48 -0500

    updated model_admin search fields

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import Book, Author, Language, Genre, BookInstance
 
 
-admin.site.register(Genre)
 admin.site.register(Language)
 
 
@@ -16,16 +15,24 @@ class BooksInstanceInline(admin.TabularInline):
     extra = 0
 
 
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    search_fields = ['first_name', 'last_name']
     inlines = [BookInline]
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+    search_fields = ['author__first_name', 'author__last_name', 'genre__name']
+    autocomplete_fields = ['author', 'genre']
     inlines = [BooksInstanceInline]
 
 
@@ -33,7 +40,9 @@ class BookAdmin(admin.ModelAdmin):
 class BookInstanceAdmin(admin.ModelAdmin):
     list_display = ('book', 'status', 'borrower', 'due_back', 'id')
     list_filter = ('status', 'due_back', 'language')
-
+    search_fields = ['book__title', 'borrower__username', 'book__author__first_name',
+                     'book__author__last_name', 'book__genre__name']
+    autocomplete_fields = ['borrower']
     fieldsets = (
         (None, {'fields': ('book', 'imprint', 'language', 'id')}),
         ('Availability', {'fields': ('status', 'due_back', 'borrower')})
